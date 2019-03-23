5bcb4bdfc2403f6c10f1a3847d1bab386187e769
ryanboris <ryan.boris@gmail.com>
2019-03-23 17:33:10 -0400

    :art: finished demo 1
    finishes demo 1

--- src/index.js
+++ src/index.js
@@ -2,31 +2,32 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = props => {
-  const [state, setState] = useState({ count: 0, text: '' })
+  const [count, setCount] = useState(props.count)
+  const [text, setText] = useState('')
 
-  const increment = () => setState({ ...state, count: state.count + 1 })
+  const increment = () => setCount(count + 1)
 
-  const decrement = () => setState({ ...state, count: state.count - 1 })
+  const decrement = () => setCount(count - 1)
 
-  const reset = () => setState({ ...state, count: 0 })
+  const reset = () => setCount(0)
 
-  const handleTextChange = e => setState({ ...state, text: e.target.value })
+  const handleTextChange = e => setText(e.target.value)
 
   return (
     <div>
       <p>
-        The current {state.text || 'count'} is {state.count}
+        The current {text || 'count'} is {count}
       </p>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <button onClick={reset}>0</button>
-      <input value={state.text} onChange={handleTextChange} />
+      <input value={text} onChange={handleTextChange} />
     </div>
   )
 }
 
 App.defaultProps = {
-  count: 10
+  count: 0
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
