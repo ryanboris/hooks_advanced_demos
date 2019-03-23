268e7d8f0151b391f7e5f0a1f0fb84384ead53a1
ryanboris <ryan.boris@gmail.com>
2019-03-22 21:37:09 -0400

    :hankey: shows an example of why trying to force React class patterns with hooks is just lame

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = props => {
-  const [count, setCount] = useState(props.count)
+  const [state, setState] = useState({ count: 0, text: '' })
 
-  const increment = () => setCount(prevCount => prevCount + 1)
+  const increment = () => setState({ ...state, count: state.count + 1 })
 
-  const decrement = () => setCount(prevCount => prevCount - 1)
+  const decrement = () => setState({ ...state, count: state.count - 1 })
 
-  const reset = () => setCount(0)
+  const reset = () => setState({ ...state, count: 0 })
+
+  const handleTextChange = e => setState({ ...state, text: e.target.value })
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>
+        The current {state.text || 'count'} is {state.count}
+      </p>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <button onClick={reset}>0</button>
+      <input value={state.text} onChange={handleTextChange} />
     </div>
   )
 }
