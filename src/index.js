f79d9257e91d54ef286cb23e745fe53480d770ae
ryanboris <ryan.boris@gmail.com>
2019-03-22 21:24:57 -0400

    :sparkles: Adds a counter demonstrating useState, and defaultProps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
+const App = props => {
+  const [count, setCount] = useState(props.count)
+
+  const increment = () => setCount(prevCount => prevCount + 1)
+
+  const decrement = () => setCount(prevCount => prevCount - 1)
+
+  const reset = () => setCount(0)
+
   return (
-    <Fragment>
-      <div>Sanity</div>
-    </Fragment>
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>0</button>
+    </div>
   )
+}
+
+App.defaultProps = {
+  count: 10
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
